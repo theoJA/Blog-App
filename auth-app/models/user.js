3d77e73c89267acbcd3cacf05af0543b409b6d0f
theoJA <theophilus.ja@gmail.com>
2017-09-04 15:52:21 -0400

    First commit. Created with create-react-app.

---
+++ auth-app/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = (id, callback) => {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+  const query = { username }
+  User.findOne(query, callback);
+}
+
+module.exports.addUSer = function(newUser, callback) {
+
+}
