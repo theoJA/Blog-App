b8ed8a766f579bc7b07b357e6dbd38e97e7bd94f
theoJA <theophilus.ja@gmail.com>
2017-09-07 23:03:19 -0400

    First commit. Comp for authentication

---
+++ auth-app/react-src/src/components/posts_auth.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class posts_auth extends Component {
+  render() {
+    return (
+      <div>
+        Nothing yet
+      </div>
+    )
+  }
+}
+
+export default posts_auth
