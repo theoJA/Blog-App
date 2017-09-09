73ab308f50ebe585df369cc22a62b8de7d290c67
theoJA <theophilus.ja@gmail.com>
2017-09-09 17:43:41 -0400

    Actions, mapStateToProps, added TextInput

--- auth-app/react-src/src/components/posts_auth.js
+++ auth-app/react-src/src/components/posts_auth.js
@@ -2,20 +2,54 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import jotLogo from "../images/jot-transparent.png"
+import { TextInput } from './common'
+import { emailChanged, passwordChanged } from '../actions/AuthActions'
 import "./posts_auth.css"
 
 class posts_auth extends Component {
+
+  onEmailChange(text) {
+    this.props.emailChanged(text)
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text)
+  }
+
   render() {
     return (
       <div className="container absoluteCenter">
         <div>
           <img src={jotLogo} />
           <h4>Write your thoughts down, anywhere, anytime.</h4>
-          <h6>Everything is centered now! YESSS!</h6>
+          <TextInput 
+            label="Email"
+            onChange={this.onEmailChange.bind(this)}
+            placeholder="email"
+            id="inputEmail"
+            type="text"
+            value={this.props.email}
+          />
+          <TextInput 
+            label="Password"
+            onChange={this.onPasswordChange.bind(this)}
+            placeholder="password"
+            id="inputPassword"
+            type="password"
+            value={this.props.password}
+          />
+          <Link className="btn btn-success spacing" to="/posts">Sign Up</Link>
+          <Link className="btn btn-primary spacing" to="/posts">Sign In</Link>
         </div>
       </div>
     )
   }
 }
 
-export default posts_auth
+const mapStateToProps = ({auth}) => {
+  const { email, password, error, loading } = auth
+  console.log(email)
+  return { email, password, error, loading }
+}
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged } )(posts_auth)
