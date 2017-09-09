c7c5d92d51033c966c28da76705df5ef450bf4c2
theoJA <theophilus.ja@gmail.com>
2017-09-08 20:24:21 -0400

    Just some ui changes

--- auth-app/react-src/src/components/common/Navbar.js
+++ auth-app/react-src/src/components/common/Navbar.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import jotLogo from '../../images/jot-transparent.png'
+import './Navbar.css'
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <Link className="navbar-brand" to="/posts">{props.navbarTitle}</Link>
-        </div>
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <Link className="navbar-brand" to="/posts"><img className="smallerLogo" src={jotLogo} /></Link>
+          </div>
 
-        <div className="collapse navbar-collapse" id="navbar">
-          <ul className="nav navbar-nav navbar-right">
-            <li><Link to="/posts">Dashboard</Link></li>
-            <li><Link to="#">Profile</Link></li>
-            <li><Link to="#">Logout</Link></li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbar">
+            <ul className="nav navbar-nav navbar-right">
+              <li><Link to="/posts">Dashboard</Link></li>
+              <li><Link to="#">Profile</Link></li>
+              <li><Link to="#">Logout</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
+
     </nav>
   )
 }
