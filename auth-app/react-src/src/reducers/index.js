e2ae0db634a7d22dc6cbd81728a1387d98f7dbf3
theoJA <theophilus.ja@gmail.com>
2017-09-09 17:43:59 -0400

    Added a new reducer

--- auth-app/react-src/src/reducers/index.js
+++ auth-app/react-src/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import PostsReducer from './reducer_posts'
+import PostsReducer from './PostsReducer'
+import AuthReducer from './AuthReducer'
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
+  auth: AuthReducer,
   posts: PostsReducer,
   form: formReducer
   // very critical to have the "form" state in the reducer. must be spelled "form"!
