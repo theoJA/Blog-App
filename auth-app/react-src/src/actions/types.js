4615992dfd9337309f37373437d3666d1d70409f
theoJA <theophilus.ja@gmail.com>
2017-09-09 17:55:30 -0400

    First commit

---
+++ auth-app/react-src/src/actions/types.js
@@ -0,0 +1,12 @@
+export const NAME_CHANGED = 'name_changed'
+export const EMAIL_CHANGED = 'email_changed'
+export const USERNAME_CHANGED = 'username_changed'
+export const PASSWORD_CHANGED = 'password_changed'
+export const LOGIN_USER_SUCCESS = 'login_user_success'
+export const LOGIN_USER_FAIL = 'login_user_fail'
+export const LOGIN_USER = 'login_user'
+
+export const FETCH_POSTS = 'fetch_posts'
+export const CREATE_POST = 'create_post'
+export const FETCH_POST = 'fetch_post'
+export const DELETE_POST = 'delete_post'
