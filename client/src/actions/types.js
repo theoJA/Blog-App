ecaf2072a2f0552a2d2950d4d0ede7cd994780f0
theoJA <theophilus.ja@gmail.com>
2017-09-20 18:38:37 -0400

    Added new types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,19 +1,19 @@
-export const NAME_CHANGED = 'name_changed'
+
 export const EMAIL_CHANGED = 'email_changed'
-export const USERNAME_CHANGED = 'username_changed'
 export const PASSWORD_CHANGED = 'password_changed'
-export const LOGIN_USER_SUCCESS = 'login_user_success'
-export const LOGIN_USER_FAIL = 'login_user_fail'
-export const LOGIN_USER = 'login_user'
+
+export const STATE_RESET = 'state_reset'
+
+export const REG_USER = 'reg_user'
+export const REG_ERROR = 'reg_error'
+
+export const AUTH_USER = 'auth_user'  
+export const UNAUTH_USER = 'unauth_user'
+export const AUTH_ERROR = 'auth_error'
 
 export const FETCH_POSTS = 'fetch_posts'
 export const CREATE_POST = 'create_post'
 export const FETCH_POST = 'fetch_post'
 export const DELETE_POST = 'delete_post'
 
-export const AUTH_USER = 'auth_user'  
-export const UNAUTH_USER = 'unauth_user'
-export const AUTH_ERROR = 'auth_error'
-export const FORGOT_PASSWORD_REQUEST = 'forgot_password_request'
-export const RESET_PASSWORD_REQUEST = 'reset_password_request'
-export const PROTECTED_TEST = 'protected_test'
+
