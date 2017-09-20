87bc4340c09c8c2b0b54a09f519704a4ea4bd12c
theoJA <theophilus.ja@gmail.com>
2017-09-20 18:41:58 -0400

    Added cors middleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/APIauth');
@@ -10,12 +11,13 @@ const app = express();
 
 // Middlewares
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
 app.use('/users', require('./routes/users'));
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
 app.listen(port);
 console.log(`Server listening at ${port}`);
